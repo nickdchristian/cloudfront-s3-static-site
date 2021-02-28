a051ddf93bef0f69631e56be7565359147114e75
Nicholas Christian <nicholasdchristian@gmail.com>
2021-02-28 12:47:40 -0500

    Use Cache Policy for not storing cache

--- functions/no-store-cache/index.py
+++
@@ -1,9 +0,0 @@
-def handler(event, context):
-    response = event["Records"][0]["cf"]["response"]
-    response["headers"]["cache-control"] = [
-        {
-            "key": "Cache-Control",
-            "value": "no-store",
-        }
-    ]
-    return response
